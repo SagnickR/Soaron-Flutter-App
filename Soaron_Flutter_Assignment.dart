fd9909ca04b7188f056c5aeaf30f6e5a5728b5c3
SagnickR <82783216+SagnickR@users.noreply.github.com>
2023-12-06 01:05:14 +0530

    Add files via upload

--- Soaron_Flutter_Assignment.dart
+++ Soaron_Flutter_Assignment.dart
@@ -17,7 +17,7 @@ class OrderTrackingScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              // Product Details
+              
               Text(
                 'Videography Drone',
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
@@ -28,28 +28,28 @@ class OrderTrackingScreen extends StatelessWidget {
               ),
               SizedBox(height: 8),
 
-              // Product ID
+              
               Align(
                 alignment: Alignment.centerRight,
                 child: Text('Product ID: 123456'),
               ),
               SizedBox(height: 20),
 
-              // Order Summary
+              
               Text(
                 'Order Summary',
                 style: TextStyle(fontWeight: FontWeight.bold),
               ),
               SizedBox(height: 8),
 
-              // Order Details
+
               _buildOrderDetail('Drone Type:', 'Videography Drone'),
               _buildOrderDetail('Category:', 'Ultra HD'),
               _buildOrderDetail('Price:', '₹1050.00'),
               _buildOrderDetail('Product ID:', '123456'),
               SizedBox(height: 20),
 
-              // Operator Info
+              
               Text(
                 'Operator Information',
                 style: TextStyle(fontWeight: FontWeight.bold),
@@ -60,14 +60,14 @@ class OrderTrackingScreen extends StatelessWidget {
               _buildOperatorInfo('Mobile:', '1234567890'),
               SizedBox(height: 20),
 
-              // Order Status
+              
               Text(
                 'Order Status',
                 style: TextStyle(fontWeight: FontWeight.bold),
               ),
               SizedBox(height: 8),
 
-              // Tracking Circles and Lines
+              
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
@@ -97,7 +97,7 @@ class OrderTrackingScreen extends StatelessWidget {
     );
   }
 
-  // Widget to display order detail with label and value aligned
+  
   Widget _buildOrderDetail(String label, String value) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -108,7 +108,7 @@ class OrderTrackingScreen extends StatelessWidget {
     );
   }
 
-  // Widget to display operator information with label and value aligned
+  
   Widget _buildOperatorInfo(String label, String value) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -119,7 +119,7 @@ class OrderTrackingScreen extends StatelessWidget {
     );
   }
 
-  // Widget to build the tracking circle
+  
   Widget _buildTrackingCircle(bool isActive) {
     return Container(
       width: 24,
@@ -131,7 +131,7 @@ class OrderTrackingScreen extends StatelessWidget {
     );
   }
 
-  // Widget to build the tracking line
+  
   Widget _buildTrackingLine() {
     return Expanded(
       child: Container(
@@ -141,7 +141,7 @@ class OrderTrackingScreen extends StatelessWidget {
     );
   }
 
-  // Widget to build the tracking text
+  
   Widget _buildTrackingText(String text) {
     return SizedBox(
       width: 100,
